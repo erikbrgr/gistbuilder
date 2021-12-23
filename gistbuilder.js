e67b599177e546dc274433457e14cf32ead2b6e2
Erik Burger <erik.burger@solera.nl>
2021-12-23 13:18:34 +0100

    CLI boilerplate

---
+++ gistbuilder.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import { program } from 'commander';
+
+const processFile = async (file) =>
+{
+}
+
+program.arguments('<file>')
+       .action(processFile)
+       .parse(process.argv);
